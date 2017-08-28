2dd505dbb7d365979ed325448c24507793b45ef0
phulong <phulong95@gmail.com>
2017-08-29 06:36:55 +0700

    Add js

--- resources/views/layout/head.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" type="text/css" href="{{asset('public/css/home.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('public/css/main.css')}}">
-<!-- <link rel="stylesheet" href="public/css/main.css"/>﻿ -->
-
