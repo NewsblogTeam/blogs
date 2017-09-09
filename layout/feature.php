02420a7bb0d81cfe50097671651d4a103587cdd4
phulong95 <phulong95@gmail.com>
2017-09-09 18:22:56 +0700

    Final feature

--- layout/feature.php
+++ layout/feature.php
@@ -224,4 +224,114 @@
   <script defer src="js/demo.js"></script>
 
 
+</html><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Feature</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+
+	 <!-- Syntax Highlighter -->
+  <link href="css/shCore.css" rel="stylesheet" type="text/css" />
+  <link href="css/shThemeDefault.css" rel="stylesheet" type="text/css" />
+  <!-- Demo CSS -->
+	<link rel="stylesheet" href="css/demo.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="flexslider.css" type="text/css" media="screen" />
+
+	<!-- Modernizr -->
+  <script src="js/modernizr.js"></script>
+
+  
+
+</head>
+
+
+<div class="widget sidebar-featured-post">
+<h3 class="side-title">Featured Posts Widget</h3>  
+      <section class="slider">
+        <div class="flexslider">
+          <ul class="slides">
+            <li class="item">
+  	    	    <img src="http://kenh14cdn.com/zoom/280_175/2017/ava-1504881112493-0-4-417-672-crop-1504881128076.png" />
+		             <div class="cp-post-content">
+						<div class="catname"><a href="#" class="catname-btn btn-orange">Sports</a></div>
+						<h3><a href="single-post.html">Dàn nữ thần 9X được đánh giá quá cao: Người xấu hơn nữ phụ, kẻ bị người qua đường dìm hàng</a></h3>
+					 </div>
+  	    	</li>
+  	    	 <li class="item">
+  	    	    <img src="http://kenh14cdn.com/zoom/420_264/2017/13254546-10206398327777295-3025405041004741471-n-1504916392637-32-0-430-638-crop-1504916843451.jpg" />
+             <div class="cp-post-content">
+				<div class="catname"><a href="#" class="catname-btn btn-orange">Sports</a></div>
+				<h3><a href="single-post.html">99er lai Pháp - Việt: Xinh không trộn lẫn, biết 4 thứ tiếng và đang du học trường tốt nhất Canada</a></h3>
+				</div>
+  	    	</li>
+
+  	    		 <li class="item">
+  	    	    <img src="http://kenh14cdn.com/zoom/280_175/2017/ava-1502899175617-3-5-370-592-crop-1502899231529.png" />
+             <div class="cp-post-content">
+				<div class="catname"><a href="#" class="catname-btn btn-orange">Sports</a></div>
+				<h3><a href="single-post.html">Biến mất đã lâu nhưng 4 hot girl này vẫn có lượt followers "khủng" trên Instagram</a></h3>
+				</div>
+  	    	</li>
+  	    		
+          </ul>
+        </div>
+        <div class="custom-navigation">
+		    <div class="cus">
+		<a href="#" class="owl-prev"><i class="glyphicon glyphicon-chevron-left"></i></a>
+		  	<a href="#" class="flex-next"><i class="glyphicon glyphicon-chevron-right"></i></a>		    	
+		    </div>   		 
+		</div>
+      </section> 
+
+  </div> <!--  End div -->
+
+   <!-- jQuery -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
+
+  <!-- FlexSlider -->
+  <script defer src="jquery.flexslider.js"></script>
+
+  <script type="text/javascript">
+
+    $(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+
+      $('.flexslider').flexslider({
+        animation: "slide",
+        controlsContainer: $(".custom-controls-container"),
+         customDirectionNav: $(".custom-navigation a")
+
+        
+      });
+    });
+
+  
+      $(document).ready(function(){
+
+   
+       	$(".flex-control-paging").css("background-color","red");
+   
+});
+  </script>
+
+  <!-- Syntax Highlighter -->
+  <script type="text/javascript" src="js/shCore.js"></script>
+  <script type="text/javascript" src="js/shBrushXml.js"></script>
+  <script type="text/javascript" src="js/shBrushJScript.js"></script>
+
+  <!-- Optional FlexSlider Additions -->
+  <script src="js/jquery.easing.js"></script>
+  <script src="js/jquery.mousewheel.js"></script>
+  <script defer src="js/demo.js"></script>
+
+
 </html>
