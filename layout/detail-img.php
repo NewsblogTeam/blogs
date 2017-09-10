15b4d7da6d91e63f3d13905b160ecedf3ef21868
phulong <phulong95@gmail.com>
2017-09-10 17:01:28 +0700

    Update phan chi tiet tin hình ảnh và Silder

---
+++ layout/detail-img.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Feature</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.css">
+	<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+
+	 <!-- Syntax Highlighter -->
+  <link href="css/shCore.css" rel="stylesheet" type="text/css" />
+  <link href="css/shThemeDefault.css" rel="stylesheet" type="text/css" />
+  <!-- Demo CSS -->
+	<link rel="stylesheet" href="css/demo.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="flexslider.css" type="text/css" media="screen" />
+
+	<!-- Modernizr -->
+  <script src="js/modernizr.js"></script>
+
+  
+
+</head>
+
+<body >
+ 
+  <!-- Deatail image -->
+       <div class="deatail-image">
+          <ul>
+          <!-- Item -->
+            <li class="cp-fullwidth-news-post-excerpt">
+                  <div class="cp-thumb"><img class="image-thump" src="http://image.24h.com.vn/upload/3-2017/images/2017-09-06/1504666457-150466108441213-anh-3.jpg" alt=""><span class="ground"></span></div>
+                  <div class="cp-post-content">
+                  <h3><a href="#">Đây là cô giáo tiểu học nổi bật nhất lễ khai giảng 2017</a></h3>
+                    <ul class="cp-post-tools">
+                        <li><i class="glyphicon glyphicon-time"></i> May 10, 2016</li>
+                        <li><i class="glyphicon glyphicon-user"></i> Roy Miller</li>
+                        <li><i class="glyphicon glyphicon-folder-open"></i> Lifestyle</li>
+                        <li><i class="glyphicon glyphicon-comment"></i> 57 Comments</li>
+                    </ul>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                  <a href="#" class="cp-readmore waves-effect waves-button">Read More <i class="fa fa-angle-right"></i></a>
+                  </div>
+            </li>    
+            <!-- End Item -->
+            <!-- Item -->
+            <li class="cp-fullwidth-news-post-excerpt">
+                  <div class="cp-thumb"><img class="image-thump" src="http://image.24h.com.vn/upload/3-2017/images/2017-09-08/1504861084-tang-can-1.jpg" alt=""><span class="ground"></span></div>
+                  <div class="cp-post-content">
+                  <h3><a href="#">Cô gái gầy tiết lộ bí quyết sau 7 năm nỗ lực tăng cân</a></h3>
+                    <ul class="cp-post-tools">
+                        <li><i class="glyphicon glyphicon-time"></i> May 10, 2016</li>
+                        <li><i class="glyphicon glyphicon-user"></i> Roy Miller</li>
+                        <li><i class="glyphicon glyphicon-folder-open"></i> Lifestyle</li>
+                        <li><i class="glyphicon glyphicon-comment"></i> 57 Comments</li>
+                    </ul>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+                  <a href="#" class="cp-readmore waves-effect waves-button">Read More <i class="fa fa-angle-right"></i></a>
+                  </div>
+            </li>    
+            <!-- End Item -->
+
+   </ul>
+         
+  </div> <!-- End Deatail img -->
+
+    
+
+  
+
+ </body>
+
+</html>
