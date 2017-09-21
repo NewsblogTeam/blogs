ca86ad05c81f1b82dda67b8f8c40c8ee07350765
GaCold <minhtoan95it@gmail.com>
2017-09-21 21:24:44 +0700

    update main

--- resources/views/admin/layout/mainlayout.blade.php
+++ resources/views/admin/layout/mainlayout.blade.php
@@ -17,7 +17,7 @@
             </div>
 
             <!--logo start-->
-            <a href="index.html" class="logo">Nice <span class="lite">Admin</span></a>
+            <a href="index.html" class="logo">Blog24<span class="lite">Admin</span></a>
             <!--logo end-->
 
             <div class="nav search-row" id="top_menu">
