826a27d1438394b3779c2d6bd5daf53b0ba621d1
GaCold <minhtoan95it@gmail.com>
2017-09-21 21:47:25 +0700

    update sidebar header

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,49 @@
+ <aside>
+  <div id="sidebar"  class="nav-collapse ">
+    <!-- sidebar menu start-->
+    <ul class="sidebar-menu">                
+      <li class="active">
+        <a class="" href="index.html">
+          <i class="icon_house_alt"></i>
+          <span>Home</span>
+        </a>
+      </li>
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+          <i class="icon_document_alt"></i>
+          <span>Quản lý bài viết</span>
+          <span class="menu-arrow arrow_carrot-right"></span>
+        </a>
+        <ul class="sub">
+          <li><a class="" href="form_component.html">Bài viết mới đăng</a></li>
+          <li><a class="" href="form_validation.html">Bài viết đang đợi duyệt</a></li>
+          <li><a class="" href="form_validation.html">Bài viết span</a></li>
+        </ul>
+      </li>       
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+          <i class="icon_desktop"></i>
+          <span>Thống kê</span>
+          <span class="menu-arrow arrow_carrot-right"></span>
+        </a>
+        <ul class="sub">
+          <li><a class="" href="general.html">Lượt truy cập</a></li>
+          <li><a class="" href="buttons.html">Lượt xem bài</a></li>
+          <li><a class="" href="grids.html">Đánh giá bài viết</a></li>
+        </ul>
+      </li>
+      <li class="sub-menu">
+        <a href="javascript:;" class="">
+          <i class="icon_documents_alt"></i>
+          <span>Profile</span>
+          <span class="menu-arrow arrow_carrot-right"></span>
+        </a>
+        <ul class="sub">                          
+          <li><a class="" href="profile.html">Cập nhật profile</a></li>
+        </ul>
+      </li>
+      
+    </ul>
+    <!-- sidebar menu end-->
+  </div>
+</aside>
